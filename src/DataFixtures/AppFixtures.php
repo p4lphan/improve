a268967b70a4e5049676b55b16aef52d94d46def
DWWM <dwwm@cefimmac28.local>
2019-03-14 17:12:53 +0100

    Changement des fixtures app

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\TypePublication;
+use App\Entity\Publication;
 
 class AppFixtures extends Fixture
 {
@@ -15,6 +16,17 @@ class AppFixtures extends Fixture
         $typePublication->setValid(true);
         
         $manager->persist($typePublication);
+        $manager->flush();
+        
+        $publication = new Publication();
+        $publication->setName('Critique');
+        $publication->setContent("C'est le bossu qui se moque du chameau");
+        $publication->setIdType($typePublication);
+        $publication->setAuthor('inconnu');
+        $publication->setCreateDate(new \DateTime);
+        $publication->setValid(true);
+        
+        $manager->persist($publication);
         $manager->flush();
     }
 }
