535df1f3d39361e4981cb4fb443481d4cccf1191
DWWM <paulphan31@gmail.com>
2019-03-18 10:08:25 +0100

    Changement des fixtures app avec utilisateur récupérer avec les références

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,42 +7,38 @@ use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\TypePublication;
 use App\Entity\Publication;
 use App\Entity\User;
+use App\DataFixtures\UserFixtures;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(['userName' => 'Admin']);
-        
+       
         $typePublication = new TypePublication();
         $typePublication->setName('Citation');
         $typePublication->setValid(true);
         
         $manager->persist($typePublication);
-        $manager->flush();
-        
         $publication = new Publication();
         $publication->setName('Critique');
         $publication->setContent("C'est le bossu qui se moque du chameau");
         $publication->setIdType($typePublication);
         $publication->setAuthor('Le bossu');
         $publication->setCreateDate(new \DateTime);
-        $publication->setUser($user);
+        $publication->setUser($this->getReference(UserFixtures::ADMIN_USER_REFERENCE));
         $publication->setValid(true);
-        
         $manager->persist($publication);
-        $manager->flush();
         
-        $publication->setName('Critique');
-        $publication->setContent("C'est le bossu qui se moque du chameau");
+        $publication->setName('Faute');
+        $publication->setContent("Faute avoué à moitié pardonnée");
         $publication->setIdType($typePublication);
-        $publication->setAuthor('Le bossu');
+        $publication->setAuthor('inconnu');
         $publication->setCreateDate(new \DateTime);
-        $publication->setUser($user);
+        $publication->setUser($this->getReference(UserFixtures::ADMIN_USER_REFERENCE));
         $publication->setValid(true);
-        
-        
         $manager->persist($publication);
+        
         $manager->flush();
+        
     }
 }
